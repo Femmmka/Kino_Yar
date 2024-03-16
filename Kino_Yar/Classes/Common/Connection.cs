e5b304a9d4e3883ea21b8817b4cf64a8d90c51d0
Ефим <fimakrut@mail.ru>
2024-03-16 14:31:56 +0500

    Создание класса для работы с БД. Ярыгин

---
+++ Kino_Yar/Classes/Common/Connection.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Kino_Yar.Classes.Common
+{
+    public class Connection
+    {
+        public static readonly string confing = "server=127.0.0.1;uid=root;pwd=;database=cinema;";
+        public MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(confing);
+            connection.Open();
+
+            return connection;
+        }
+        public MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+        public void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
